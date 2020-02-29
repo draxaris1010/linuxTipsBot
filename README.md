ea08b107654c6f2f8cafa0c010fd43a907132f5c
draxaris1010 <axel.dezittere@gmail.com>
2020-02-29 17:32:40 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-linuxTipsBot
+# linuxTipsBot
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python3
+from flask import Flask, request, jsonify
+
+from telegram_bot import TelegramBot
+from config import TELEGRAM_INIT_WEBHOOK_URL
 print("Hello world!");
